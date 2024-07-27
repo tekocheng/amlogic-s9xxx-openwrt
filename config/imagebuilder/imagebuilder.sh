07faf5faae72abc4e1ca97a83134259145918217
tekocheng <48650357+tekocheng@users.noreply.github.com>
2024-07-27 21:53:47 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -179,16 +179,29 @@ rebuild_firmware() {
 
     # Selecting default packages, lib, theme, app and i18n, etc.
     my_packages="\
-        cgi-io libiwinfo libiwinfo-data libiwinfo-lua liblua liblucihttp liblucihttp-lua \
-        libubus-lua lua luci-base luci-lib-base luci-lib-ip luci-lib-jsonc luci-lib-nixio \
-        luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system luci-proto-ppp luci-ssl \
-        luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-qmi luci-proto-relay \
-        luci-compat luci-lib-ipkg rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci rpcd-mod-rrdns \
-        hostapd hostapd-utils kmod-ath kmod-ath9k kmod-ath9k-common kmod-ath9k-htc \
+        acpid attr base-files bash bc blkid block-mount blockd bsdtar btrfs-progs busybox bzip2 \
+        cgi-io chattr comgt comgt-ncm coremark coreutils coreutils-base64 coreutils-nohup \
+        coreutils-stat coreutils-truncate curl dosfstools dumpe2fs \ 
+        e2freefrag e2fsprogs exfat-mkfs f2fs-tools f2fsck fdisk \
+        gawk getopt git gzip hostapd-common iconv iw iwinfo jq jshn \
+        kmod-ath kmod-ath9k kmod-ath9k-common kmod-ath9k-htc kmod-brcmfmac kmod-brcmutil \
         kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash kmod-fs-btrfs \
-        kmod-mac80211 wireless-tools wpa-cli wpa-supplicant libiwinfo-data wireless-regdb usb-modeswitch \
-        openssh-sftp-server usbutils kmod-usb2 kmod-brcmfmac perlbase-file kmod-brcmutil perlbase-essential \
-        perlbase-utf8 perlbase-file perlbase-time perlbase-xsloader coreutils-stat zoneinfo-asia \
+        kmod-mac80211 kmod-usb2 libiwinfo libiwinfo-data libiwinfo-lua libjson-script liblua \
+        liblucihttp liblucihttp-lua libubus-lua losetup lsattr lsblk lscpu lua \
+        luci luci-base luci-compat luci-lib-base luci-lib-ip luci-lib-ipkg \
+        luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system \
+        \
+        luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm \
+        luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay luci-ssl \
+        \
+        mkf2fs mount-utils openssh-sftp-server openssl-util parted \
+        perl-http-date perlbase-essential perlbase-file perlbase-getopt \
+        perlbase-time perlbase-unicode perlbase-utf8 perlbase-xsloader \
+        pigz ppp ppp-mod-pppoe proto-bonding pv rename resize2fs \
+        rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci rpcd-mod-rrdns \
+        runc tar tini tune2fs uclient-fetch uhttpd uhttpd-mod-ubus unzip uqmi usb-modeswitch usbutils uuidgen \
+        wget-ssl whereis which wireless-regdb wireless-tools wpad-basic wwan xfs-fsck xfs-mkfs \
+        xz xz-utils ziptool zoneinfo-asia zoneinfo-core zstd \
         \
         luci-app-amlogic \
         \
